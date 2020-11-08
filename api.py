ac247f06d7dee5b9aea765ece429710fc9ccddba
Isa Conteh <iconteh21@gmail.com>
2020-11-08 17:58:22 -0500

    added not working code for find function

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# Create some test data for our catalog in the form of a list of dictionaries. 
 books = [
     {'id': 0,
      'title': 'A Fire Upon the Deep',
@@ -29,5 +30,28 @@ def home():
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
-    
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL. 
+    # If ID is provided, assign it to a variable. 
+    # If no ID is provided, display an error in the browser 
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for results 
+    results = []
+
+    # Loop through the data and match results that fit the requested ID. 
+    # IDs are unique, but other fields might return many results 
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert out list of 
+    # Python dictionaries to the JSON format 
+    return jsonify(results)
+
 app.run()
